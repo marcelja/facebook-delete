1e6f8d7132b786be297ab388a3a0398200713b22
Marcel Jankrift <marcel@jankrift.de>
2021-10-20 12:50:13 +0200

    Add ARM arch to build script (closes #34)

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-GOOS=linux go build -o deleter-linux
-GOOS=darwin go build -o deleter-macos
-GOOS=windows go build -o deleter-windows.exe
+GOOS=linux GOARCH=amd64 go build -o deleter-linux
+GOOS=linux GOARCH=arm64 go build -o deleter-linux-arm64
+GOOS=darwin GOARCH=amd64 go build -o deleter-macos
+GOOS=darwin GOARCH=arm64 go build -o deleter-macos-arm64
+GOOS=windows GOARCH=amd64 go build -o deleter-windows.exe
+GOOS=windows GOARCH=arm64 go build -o deleter-windows-arm64.exe
