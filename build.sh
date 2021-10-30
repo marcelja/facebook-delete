7e4894fd79d4a06d28eaf2358e75213b801e5ec7
Marcel Jankrift <marcel@jankrift.de>
2021-10-30 09:54:54 +0200

    Enable -trimpath option for build commands to remove system file paths from binaries

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-GOOS=linux GOARCH=amd64 go build -o deleter-linux
-GOOS=linux GOARCH=arm64 go build -o deleter-linux-arm64
-GOOS=darwin GOARCH=amd64 go build -o deleter-macos
-GOOS=darwin GOARCH=arm64 go build -o deleter-macos-arm64
-GOOS=windows GOARCH=amd64 go build -o deleter-windows.exe
-GOOS=windows GOARCH=arm64 go build -o deleter-windows-arm64.exe
+GOOS=linux GOARCH=amd64 go build -trimpath -o deleter-linux
+GOOS=linux GOARCH=arm64 go build -trimpath -o deleter-linux-arm64
+GOOS=darwin GOARCH=amd64 go build -trimpath -o deleter-macos
+GOOS=darwin GOARCH=arm64 go build -trimpath -o deleter-macos-arm64
+GOOS=windows GOARCH=amd64 go build -trimpath -o deleter-windows.exe
+GOOS=windows GOARCH=arm64 go build -trimpath -o deleter-windows-arm64.exe
