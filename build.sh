0590b9e55503be047f76d0812622c260abe82599
Marcel Jankrift <marcel@jankrift.de>
2022-01-06 23:13:49 +0100

    Put binary files into build/ and add this directory to gitignore

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-GOOS=linux GOARCH=amd64 go build -trimpath -o deleter-linux
-GOOS=linux GOARCH=arm64 go build -trimpath -o deleter-linux-arm64
-GOOS=darwin GOARCH=amd64 go build -trimpath -o deleter-macos
-GOOS=darwin GOARCH=arm64 go build -trimpath -o deleter-macos-arm64
-GOOS=windows GOARCH=amd64 go build -trimpath -o deleter-windows.exe
-GOOS=windows GOARCH=arm64 go build -trimpath -o deleter-windows-arm64.exe
+GOOS=linux GOARCH=amd64 go build -trimpath -o build/deleter-linux
+GOOS=linux GOARCH=arm64 go build -trimpath -o build/deleter-linux-arm64
+GOOS=darwin GOARCH=amd64 go build -trimpath -o build/deleter-macos
+GOOS=darwin GOARCH=arm64 go build -trimpath -o build/deleter-macos-arm64
+GOOS=windows GOARCH=amd64 go build -trimpath -o build/deleter-windows.exe
+GOOS=windows GOARCH=arm64 go build -trimpath -o build/deleter-windows-arm64.exe
