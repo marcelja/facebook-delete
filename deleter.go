90f05750eec148bcb81f2ff2a322d924593474b7
Marcel Jankrift <marcel@jankrift.de>
2024-01-01 13:07:11 +0100

    add 2024

--- deleter.go
+++ deleter.go
@@ -555,7 +555,7 @@ func main() {
 
 	if customYearsFlagValid {
 		if customYears == "all" {
-			customYears = "2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006"
+			customYears = "2024,2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006"
 		}
 	} else {
 		fmt.Println("Invalid years passed through customYears flag.")
